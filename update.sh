f31fd6943cc0f2fbe4e6abc22bc8524c5bc44c85
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-02-14 16:57:06 +0100

    Update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+filestring=`curl -s https://github.com/bh107/bohrium/releases | grep "archive.*gz" | head -n 1 | cut -d '"' -f2`
+url="https://github.com$filestring"
+wget -q "$url" -O release.tar.gz
+sha=`shasum -a 256 release.tar.gz | cut -d ' ' -f1`
+rm release.tar.gz
+
+url=${url//\//\\\/}
+perl -i -pe "s/url .*?$/url \"$url\"/g" Formula/bohrium.rb
+perl -i -pe "s/sha256 .*?$/sha256 \"$sha\"/g" Formula/bohrium.rb
